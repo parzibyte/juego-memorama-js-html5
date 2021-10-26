56b77b93d7494859ebee54faf0ee0c0f4008313e
Luis Cabrera Benito <parzibyte@gmail.com>
2021-10-25 20:08:06 -0500

    Cambiando imagen de "oculto"

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const PREFIJO_IMAGENES = "./imagenes/", // Como las imágenes se cargan desde un
     MAXIMOS_INTENTOS = 8, // Intentos máximos que tiene el jugador
     COLUMNAS = 4, // Columnas del memorama
     SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 1, // Por cuántos segundos mostrar ambas imágenes
-    NOMBRE_IMAGEN_OCULTA = "./img/question.png"; // La imagen que se muestra cuando la real está oculta
+    NOMBRE_IMAGEN_OCULTA = "./img/question.gif"; // La imagen que se muestra cuando la real está oculta
 new Vue({
     el: "#app",
     data: () => ({
